37ef8309320036db7cf5ba6c25c4b6531705493b
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-11-24 21:45:57 +0800

    Setup backend
    The project is set up with folders, npm, and all needed packages like Express, Mongoose, Dotenv, BcryptJS, and JWT. It includes a .env file, a working database connection in db.js, and the server has been tested and is running correctly.

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { connectDB } from "../Backend/config/db.js";
+import dotenv from 'dotenv';
 
 const app = express();
 
+//LOAD DOTENV VAIRABLES
+dotenv.config();
+
+//DB CONNECTION
+connectDB();
+
+//TEST SERVER
+app.listen(5000, (err) => {
+  if (!err) {
+    console.log("Server started")
+  }
+  else {
+    console.log("Server failed")
+  }
+});
+
+//TEST GET 
 app.get("/", (req, res) => {
-  res.send("Server Ready")
+  try {
+    res.send('Server Ready')
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
 })
