78f7d462204afb9311f7f7ef10a4c9e3e11f894f
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-12-08 22:57:16 +0800

    get, delete, and update todo items complete
    Created controllers and routes for get, delete, and update of todo items.
    
    Testing in postman successful

--- Backend/controller/todo.controller.js
+++ Backend/controller/todo.controller.js
@@ -1,3 +1,4 @@
+import { get } from "mongoose";
 import Todo from "../models/todo.model.js";
 
 // POST
@@ -12,33 +13,48 @@ export const createTodoItem = async (req, res) => {
     const savedTodo = await newTodo.save();
     res.status(201).json(savedTodo);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 }
 
 // GET
 export const getTodoItem = async (req, res) => {
   try {
-
+    const getTodo = await Todo.find({ userId: req.userId });
+    res.status(200).json(getTodo);
   } catch (error) {
-
+    res.status(500).json({ message: error.message })
   }
 }
 
 // DELETE
 export const deleteTodoItem = async (req, res) => {
   try {
-
+    const deletedTodo = await Todo.findByIdAndDelete({
+      _id: req.params.id,
+      userId: req.userId
+    })
+    if (!deletedTodo) return res.status(404).json({ message: "Item not found" });
+    res.status(200).json(deletedTodo);
   } catch (error) {
-
+    res.status(500).json({ message: error.message });
   }
 }
 
 // UPDATE
 export const updateTodoItem = async (req, res) => {
   try {
-
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      {
+        _id: req.params.id,
+        userId: req.userId
+      },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!updatedTodo) return res.status(404).json({ message: "Item not found" })
+    res.status(200).json(updatedTodo)
   } catch (error) {
-
+    res.status(500).json({ message: error.message });
   }
 }
--- Backend/routes/todo.routes.js
+++ Backend/routes/todo.routes.js
@@ -1,9 +1,15 @@
 import express from "express"
 import { authMiddleware } from "../middleware.js";
-import { createTodoItem } from "../controller/todo.controller.js"
+import { createTodoItem, deleteTodoItem, getTodoItem, updateTodoItem } from "../controller/todo.controller.js"
 
 const router = express.Router();
 
-router.post("/create", authMiddleware, createTodoItem)
+router.post("/create", authMiddleware, createTodoItem);
+
+router.get("/get", authMiddleware, getTodoItem);
+
+router.delete("/delete/:id", authMiddleware, deleteTodoItem);
+
+router.put("/update/:id", authMiddleware, updateTodoItem);
 
 export default router
